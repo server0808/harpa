1d5a6f40e2ae365681fdf4a6516fe5f7f9d60a28
Vinicio <almeida.vinicio@gmail.com>
2024-02-17 06:36:05 -0300

    second

--- app.py
+++ app.py
@@ -8,105 +8,246 @@ import plotly.graph_objects as go
 from datetime import datetime
 import math
 from scipy import stats
+from scipy.stats import norm
 from scipy.optimize import newton
 
+st.markdown("# Harpa Quant")
+st.markdown("[![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white)](https://twitter.com/harpaquant)")
+st.markdown("[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white)](https://www.instagram.com/harpaquant)")
+
+st.markdown('---')
+
+st.sidebar.markdown("""
+    Estudos quantitativos para auxiliar a tomada de decisão de investidores. 
+    Atualmente, disponibilizando ferramentas para o mercado de derivativos. 
+    Acesse nossa [Comunidade no Discord](https://discord.gg/MaF7wZDQvZ) para participar de discussões no tema. Email: harpaquant@gmail.com
+    """)
+
+st.sidebar.markdown('---')
+
+selected_calculator = st.sidebar.selectbox(
+    "Selecione a ferramenta:",
+    ("Calculadoras Black-Scholes-Merton", "Calculadora de Gregas de Opções", "Put Call Ratio - PCR", "Cones de Volatilidade")
+)
+
+st.sidebar.markdown('---')
 st.sidebar.subheader('Ferramentas disponíveis')
 st.sidebar.write('Calculadoras Black-Scholes-Merton \n\n- Preço da opção\n\n- Volatilidade implícita')
+st.sidebar.write('Calculadora de Gregas de Opções')
+st.sidebar.write('Put Call Ratio - PCR')
+st.sidebar.write('Cones de Volatilidade')
 
-#st.image('/home/vinicio/Dropbox/Fortem/GitHub/harpa/harpa2.jpg', width=250)
-st.title('Harpa Quant')
-st.text('Estudos quantitativos para auxiliar a tomada de decisão de investidores.') 
-
-st.text('Atualmente, disponibilizando ferramentas para o mercado de derivativos.')
-
-st.markdown("[Acesse nossa comunidade no Discord para participar de discussões no tema.](https://discord.gg/MaF7wZDQvZ)")
-st.markdown('---')
-
-### Calculadora Black-Scholes-Merton - Preço
-def black_scholes_call_put(S, K, T, r, sigma, option_type):
-    d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
-    d2 = d1 - sigma * math.sqrt(T)
+if selected_calculator == "Calculadoras Black-Scholes-Merton":
     
-    if option_type == 'call':
-        option_price = S * norm_cdf(d1) - K * math.exp(-r * T) * norm_cdf(d2)
-    elif option_type == 'put':
-        option_price = K * math.exp(-r * T) * norm_cdf(-d2) - S * norm_cdf(-d1)
-    else:
-        raise ValueError("Invalid option type. Use 'call' or 'put'.")
-    
-    return option_price
+    ### Calculadora Black-Scholes-Merton - Preço
+    def black_scholes_call_put(S, K, T, r, sigma, option_type):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+        d2 = d1 - sigma * math.sqrt(T)
+        
+        if option_type == 'call':
+            option_price = S * norm_cdf(d1) - K * math.exp(-r * T) * norm_cdf(d2)
+        elif option_type == 'put':
+            option_price = K * math.exp(-r * T) * norm_cdf(-d2) - S * norm_cdf(-d1)
+        else:
+            raise ValueError("Invalid option type. Use 'call' or 'put'.")
+        
+        return option_price
 
-# Função de distribuição cumulativa normal padrão (CDF)
-def norm_cdf(x):
-    return (1.0 + math.erf(x / math.sqrt(2.0))) / 2.0
+    # Função de distribuição cumulativa normal padrão (CDF)
+    def norm_cdf(x):
+        return (1.0 + math.erf(x / math.sqrt(2.0))) / 2.0
 
-# Título do aplicativo
-st.subheader('Calculadora Black-Scholes-Merton - Preço da opção')
+    # Título do aplicativo
+    st.subheader('Calculadora Black-Scholes-Merton - Preço da opção')
+    st.markdown("""
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit.
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    """)
+    # Organizando os campos de entrada em duas colunas
+    col1, col2 = st.columns(2)
 
-# Organizando os campos de entrada em duas colunas
-col1, col2 = st.columns(2)
+    # Entrada dos valores dos parâmetros
+    with col1:
+        S = st.number_input('Preço do ativo subjacente (S)', min_value=0.0)
+        K = st.number_input('Preço de exercício da opção (K)', min_value=0.0)
+        T = st.number_input('Vencimento da opção (T), como fração do ano', min_value=0.0)
+    with col2:
+        r = st.number_input('Taxa de juros anual (r), como fração', min_value=0.0)
+        sigma = st.number_input('Volatilidade (Sigma), como fração', min_value=0.0)
+        option_type = st.selectbox('Tipo de opção', ['call', 'put'])
 
-# Entrada dos valores dos parâmetros
-with col1:
-    S = st.number_input('S - preço spot do ativo', min_value=0.0)
-    K = st.number_input('K - preço de exercício da opção', min_value=0.0)
-    T = st.number_input('T - vencimento da opção, como fração do ano', min_value=0.0)
-with col2:
-    r = st.number_input('r - taxa de juros anual, como fração', min_value=0.0)
-    sigma = st.number_input('Sigma - volatilidade, como fração', min_value=0.0)
-    option_type = st.selectbox('Tipo de opção', ['call', 'put'])
+    # Botão para calcular o preço da opção
+    if st.button('Calcular preço da opção'):
+        option_price = black_scholes_call_put(S, K, T, r, sigma, option_type)
+        st.write(f'O preço da {option_type} é: ${round(option_price,2)}')
 
-# Botão para calcular o preço da opção
-if st.button('Calcular preço da opção'):
-    option_price = black_scholes_call_put(S, K, T, r, sigma, option_type)
-    st.write(f'O preço da {option_type} é: ${round(option_price,2)}')
+    st.markdown('---')
 
-st.markdown('---')
+    # Função para calcular o preço da opção Black-Scholes-Merton
+    def black_scholes_call_put(S, K, T, r, sigma, option_type):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+        d2 = d1 - sigma * math.sqrt(T)
+        
+        if option_type == 'call':
+            option_price = S * stats.norm.cdf(d1) - K * math.exp(-r * T) * stats.norm.cdf(d2)
+        elif option_type == 'put':
+            option_price = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * stats.norm.cdf(-d1)
+        else:
+            raise ValueError("Invalid option type. Use 'call' or 'put'.")
+        
+        return option_price
 
-# Função para calcular o preço da opção Black-Scholes-Merton
-def black_scholes_call_put(S, K, T, r, sigma, option_type):
-    d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
-    d2 = d1 - sigma * math.sqrt(T)
-    
-    if option_type == 'call':
-        option_price = S * stats.norm.cdf(d1) - K * math.exp(-r * T) * stats.norm.cdf(d2)
-    elif option_type == 'put':
-        option_price = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * stats.norm.cdf(-d1)
-    else:
-        raise ValueError("Invalid option type. Use 'call' or 'put'.")
-    
-    return option_price
+    # Função para calcular a volatilidade implícita
+    def implied_volatility(option_price, S, K, T, r, option_type):
+        # Função para encontrar a volatilidade implícita usando o método de Newton-Raphson
+        def func(sigma, option_price, S, K, T, r, option_type):
+            return black_scholes_call_put(S, K, T, r, sigma, option_type) - option_price
 
-# Função para calcular a volatilidade implícita
-def implied_volatility(option_price, S, K, T, r, option_type):
-    # Função para encontrar a volatilidade implícita usando o método de Newton-Raphson
-    def func(sigma, option_price, S, K, T, r, option_type):
-        return black_scholes_call_put(S, K, T, r, sigma, option_type) - option_price
+        # Chamada para o método de Newton-Raphson para encontrar a volatilidade implícita
+        sigma = newton(func, x0=0.2, args=(option_price, S, K, T, r, option_type))
+        return sigma
 
-    # Chamada para o método de Newton-Raphson para encontrar a volatilidade implícita
-    sigma = newton(func, x0=0.2, args=(option_price, S, K, T, r, option_type))
-    return sigma
+    # Título do aplicativo
+    st.subheader('Calculadora Black-Scholes-Merton - Volatilidade implícita')
+    st.markdown("""
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit.
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+    """)
+    # Organizando os campos de entrada em duas colunas
+    col1, col2 = st.columns(2)
 
-# Título do aplicativo
-st.subheader('Calculadora Black-Scholes-Merton - Volatilidade implícita')
+    # Entrada dos valores dos parâmetros
+    with col1:
+        Sv = st.number_input('Preço do ativo subjacente (S)', min_value=0.0, key='Sv')
+        Kv = st.number_input('Preço de exercício da opção (K)', min_value=0.0, key='Kv')
+        Tv = st.number_input('Vencimento da opção (T), como fração do ano', min_value=0.0, key='Tv')
+    with col2:
+        rv = st.number_input('Taxa de juros anual (r), como fração', min_value=0.0, key='rv')
+        option_price = st.number_input('Preço da opção', min_value=0.0, key='option_price')
+        option_type = st.selectbox('Tipo de opção', ['call', 'put'], key='option_type')
 
-# Organizando os campos de entrada em duas colunas
-col1, col2 = st.columns(2)
+    # Botão para calcular a volatilidade implícita
+    if st.button('Calcular volatilidade implícita'):
+        sigma_impl = implied_volatility(option_price, Sv, Kv, Tv, rv, option_type)
+        st.write(f'A volatilidade implícita é {round(sigma_impl,2)}')
 
-# Entrada dos valores dos parâmetros
-with col1:
-    Sv = st.number_input('S - preço spot do ativo', min_value=0.0, key='Sv')
-    Kv = st.number_input('K - preço de exercício da opção', min_value=0.0, key='Kv')
-    Tv = st.number_input('T - vencimento da opção, como fração do ano', min_value=0.0, key='Tv')
-with col2:
-    rv = st.number_input('r - taxa de juros anual, como fração', min_value=0.0, key='rv')
-    option_price = st.number_input('Preço da opção', min_value=0.0, key='option_price')
-    option_type = st.selectbox('Tipo de opção', ['call', 'put'], key='option_type')
+elif selected_calculator == "Calculadora de Gregas de Opções":
+    # Calculadora de gregas
+    # Função para calcular a grega Delta
+    def delta(S, K, T, r, sigma, option_type):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
 
-# Botão para calcular a volatilidade implícita
-if st.button('Calcular volatilidade implícita'):
-    sigma_impl = implied_volatility(option_price, Sv, Kv, Tv, rv, option_type)
-    st.write(f'A volatilidade implícita é {round(sigma_impl,2)}')
+        if option_type == 'call':
+            delta = norm.cdf(d1)
+        elif option_type == 'put':
+            delta = norm.cdf(d1) - 1
+        else:
+            raise ValueError("Invalid option type. Use 'call' or 'put'.")
+        
+        return delta
 
-st.markdown('---')
+    # Função para calcular a grega Gamma
+    def gamma(S, K, T, r, sigma):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+        gamma = norm.pdf(d1) / (S * sigma * math.sqrt(T))
+        return gamma
 
+    # Função para calcular a grega Vega
+    def vega(S, K, T, r, sigma):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+        vega = S * norm.pdf(d1) * math.sqrt(T)
+        return vega
+
+    # Função para calcular a grega Theta
+    def theta(S, K, T, r, sigma, option_type):
+        d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+        d2 = d1 - sigma * math.sqrt(T)
+
+        if option_type == 'call':
+            theta = -(S * norm.pdf(d1) * sigma) / (2 * math.sqrt(T)) - r * K * math.exp(-r * T) * norm.cdf(d2)
+        elif option_type == 'put':
+            theta = -(S * norm.pdf(d1) * sigma) / (2 * math.sqrt(T)) + r * K * math.exp(-r * T) * norm.cdf(-d2)
+        else:
+            raise ValueError("Invalid option type. Use 'call' or 'put'.")
+        
+        return theta
+
+    # Função para calcular a grega Rho
+    def rho(S, K, T, r, sigma, option_type):
+        d2 = (math.log(S / K) + (r - 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+
+        if option_type == 'call':
+            rho = K * T * math.exp(-r * T) * norm.cdf(d2)
+        elif option_type == 'put':
+            rho = -K * T * math.exp(-r * T) * norm.cdf(-d2)
+        else:
+            raise ValueError("Invalid option type. Use 'call' or 'put'.")
+        
+        return rho
+
+    # Título do aplicativo
+    st.subheader('Calculadora de Gregas de Opções')
+    st.markdown("""
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit.
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        """)
+    # Organizando os campos de entrada em duas colunas
+    col1, col2 = st.columns(2)
+
+    # Entrada dos valores dos parâmetros
+    with col1:
+        S = st.number_input('Preço do ativo subjacente (S)', min_value=0.0, step=0.01, key='S_gregas')
+        K = st.number_input('Preço de exercício da opção (K)', min_value=0.0, step=0.01, key='K_gregas')
+        T = st.number_input('Tempo até o vencimento (T) em anos', min_value=0.0, step=0.01, key='T_gregas')
+    with col2:
+        r = st.number_input('Taxa de juros anual (r), como fração', min_value=0.0, step=0.0001, key='r_gregas')
+        sigma = st.number_input('Volatilidade (Sigma), como fração', min_value=0.0, step=0.0001, key='sigma_gregas')
+        option_type = st.selectbox('Tipo de opção', ['call', 'put'], key='option_type_gregas')
+
+    # Botão para calcular as "gregas"
+    if st.button('Calcular Gregas'):
+        st.write("Delta:", round(delta(S, K, T, r, sigma, option_type),6))
+        st.write("Gamma:", round(gamma(S, K, T, r, sigma),6))
+        st.write("Vega:", round(vega(S, K, T, r, sigma),6))
+        st.write("Theta:", round(theta(S, K, T, r, sigma, option_type),6))
+        st.write("Rho:", round(rho(S, K, T, r, sigma, option_type),6))
+
+elif selected_calculator == "Put Call Ratio - PCR":
+    # Evolução de Put Call Ratio
+
+    # Título do aplicativo
+    st.subheader('Put Call Ratio - PCR')
+    st.markdown("""
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit.
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        """)
+
+elif selected_calculator == "Cones de Volatilidade":
+    # Evolução de Put Call Ratio
+
+    # Título do aplicativo
+    st.subheader('Cones de Volatilidade')
+    st.markdown("""
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit.
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        Este é um exemplo de bloco de texto usando Markdown no Streamlit. 
+        """)

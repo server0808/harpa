09624ef0a1ce7a2507412f6b316fc516d2f3d6c3
Vinicio Almeida <121837497+vinicioalmeida@users.noreply.github.com>
2024-08-05 06:18:42 -0300

    58

--- app.py
+++ app.py
@@ -273,8 +273,8 @@ elif selected_calculator == "Top 10 Fundos Quantitativos":
     st.markdown('---')
 
     # Fetch - MUDAR OS DOIS quando virar o mês
-    arquivo = 'inf_diario_fi_202407.csv'
-    link = 'https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202407.zip'
+    arquivo = 'inf_diario_fi_202408.csv'
+    link = 'https://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202408.zip'
 
     r = requests.get(link)
     zf = zipfile.ZipFile(io.BytesIO(r.content))
